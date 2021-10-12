1b7cfa0fb12ce95b60cca3bad686150d9e8abf87
valen <maryvalen142001@gmail.ocm>
2021-10-12 04:36:47 -0400

    updated landing

--- src/Components/Homepage/Landing.jsx
+++ src/Components/Homepage/Landing.jsx
@@ -12,6 +12,7 @@ import Teamzener from "src/Assets/zener.jpg";
 import Teamklyde from "src/Assets/klyde.jpg";
 import Blog1 from "src/Assets/blog1.jpg";
 import Blog2 from "src/Assets/blog2.jpg";
+import Blog4 from "src/Assets/blog4.jpg";
 import Blog3 from "src/Assets/blog3.jpg";
 import Blog5 from "src/Assets/blog5.jpg";
 import Blog6 from "src/Assets/blog6.jpg";
@@ -31,8 +32,8 @@ const Landing = () => {
       <nav className="bg-white">
         <div className="container px-6 py-2 mx-auto md:flex md:justify-between md:items-center">
           <div className="flex items-center justify-start">
-            <img src={Logo} alt="" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
-            <div className="ml-3 text-2xl font-bold text-tiffany">Cloud Shipping</div>
+            <img src={Logo} alt="" className="w-24 h-10 ml-0 flex-shrink-0 object-cover object-center" />
+            <div className="text-2xl font-bold text-tiffany">Cloud Shipping</div>
           </div>
           <div className="items-center md:flex">
             <div className="flex flex-col md:flex-row md:mx-6">
@@ -213,15 +214,9 @@ const Landing = () => {
                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                       </svg>
                     </a>
-                    <a href="https://twitter.com/cloud_shipping" className="ml-2 text-tiffany-light">
+                    <a href="https://github.com/Klylylydeee/tps-cloud-shipping-fe" className="ml-2 text-tiffany-light">
                       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                      </svg>
-                    </a>
-                    <a href="https://twitter.com/cloud_shipping" className="ml-2 text-tiffany-light">
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                       </svg>
                     </a>
                   </span>
@@ -241,15 +236,9 @@ const Landing = () => {
                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                       </svg>
                     </a>
-                    <a href="https://twitter.com/valeeeenx" className="ml-2 text-tiffany-light">
+                    <a href="https://github.com/valen142001" className="ml-2 text-tiffany-light">
                       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                      </svg>
-                    </a>
-                    <a href="https://www.instagram.com/valennnn.x/" className="ml-2 text-tiffany-light">
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                       </svg>
                     </a>
                   </span>
@@ -269,15 +258,9 @@ const Landing = () => {
                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                       </svg>
                     </a>
-                    <a href="https://twitter.com/Keeejac?fbclid=IwAR3VcN_dJhBMmQOh8hIm--diCAH-AECBP3Emx4MY9rdGJzfdM4OJuGA5-pY" className="ml-2 text-tiffany-light">
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
-                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                      </svg>
-                    </a>
-                    <a href="https://www.instagram.com/keeejac/?fbclid=IwAR3_rjAzZYkWjf24s-g8Fy4WWpw8xiS9JB6SpNQrnFN2vvRgEHg2RgZeluY" className="ml-2 text-tiffany-light">
+                    <a href="https://github.com/Klylylydeee/tps-cloud-shipping-fe" className="ml-2 text-tiffany-light">
                       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                       </svg>
                     </a>
                   </span>
@@ -297,15 +280,9 @@ const Landing = () => {
                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                       </svg>
                     </a>
-                    <a href="https://twitter.com/cloud_shipping" className="ml-2 text-tiffany-light">
+                    <a href="https://github.com/Klylylydeee/tps-cloud-shipping-fe" className="ml-2 text-tiffany-light">
                       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                      </svg>
-                    </a>
-                    <a href="https://twitter.com/cloud_shipping" className="ml-2 text-tiffany-light">
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                       </svg>
                     </a>
                   </span>
@@ -325,15 +302,9 @@ const Landing = () => {
                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                       </svg>
                     </a>
-                    <a href="https://twitter.com/cloud_shipping" className="ml-2 text-tiffany-light">
+                    <a href="https://github.com/Klylylydeee/tps-cloud-shipping-fe" className="ml-2 text-tiffany-light">
                       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                      </svg>
-                    </a>
-                    <a href="https://twitter.com/cloud_shipping" className="ml-2 text-tiffany-light">
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                       </svg>
                     </a>
                   </span>
@@ -385,7 +356,7 @@ const Landing = () => {
             </div>
             <div className="p-4 md:w-1/3">
               <div className="h-full rounded-lg overflow-hidden">
-                <img src={Blog1} alt="" className="object-contain h-30 w-30 " />
+                <img src={Blog4} alt="" className="object-contain h-30 w-30 " />
                 <div className="p-6">
                   <p className="text-lg title-font font-medium font-bold text-dark-gray mb-3">Top 3 Reasons To Choose Cloud Shipping</p>
                   <p className="tracking-widest  title-font text-xs font-medium text-gray-900 mb-1">October 1, 2021</p>
@@ -701,8 +672,8 @@ const Landing = () => {
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col  dark:bg-gray-800">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <img src={Logo} alt="" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
-              <span className="ml-3 text-xl">Cloud Shipping</span>
+              <img src={Logo} alt="" className="w-24 h-13 flex-shrink-0 object-cover object-center" />
+              <span className="text-xl">Cloud Shipping</span>
             </div>
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
@@ -713,23 +684,14 @@ const Landing = () => {
                   <Link to="/contact" className="text-gray-600 hover:text-tiffany">Contact</Link>
                 </li>
                 <li>
+                  <Link to="/service" className="text-gray-600 hover:text-tiffany">Service</Link>
+                </li>
+                <li>
                   <Link to="/team" className="text-gray-600 hover:text-tiffany">Team</Link>
                 </li>
                 <li>
                   <Link to="/become-a-partner" className="text-gray-600 hover:text-tiffany">Partner</Link>
                 </li>
-              </nav>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Services</h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <Link to="/service" className="text-gray-600 hover:text-tiffany">Package Delivery</Link>
-                </li>
-                <li>
-                  <Link to="/service" className="text-gray-600 hover:text-tiffany">Delivery within Manila</Link>
-                </li>
-
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
@@ -764,6 +726,24 @@ const Landing = () => {
         </div>
         <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <p className="text-gray-500 text-lg text-center sm:text-left">© 2021 Cloudshipping — @cloudshipping</p>
+          <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <a href="https://www.facebook.com/profile.php?id=100073132796934" className="text-dark-gray">
+              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a href="https://twitter.com/cloud_shipping" className="ml-4 text-dark-gray">
+              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a href="https://www.instagram.com/cloud.shipping/" className="ml-4 text-dark-gray">
+              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </svg>
+            </a>
+          </span>
         </div>
       </footer>
     </div>
